f1b3fb9d6c86cb9bc4f67e3e9e17a42ef6691745
Ruber Hernandez <ruberhenandez@gmail.com>
2021-04-06 12:18:41 -0500

    actualiza configuracion idioma

--- checked.js
+++ checked.js
@@ -1,12 +1,43 @@
-console.log("cargaando pagina");
-speechSynthesis.speak(new SpeechSynthesisUtterance("Bienvenido. y gracias por usarme")); 
 var lastText = "";
+var Google = 0;
 var msg = new SpeechSynthesisUtterance();
 var voices = window.speechSynthesis.getVoices();
-msg.voice = voices[5];
-msg.volume = 1; // 0 to 1
+console.log("cargando pagina");
+speechSynthesis.speak(new SpeechSynthesisUtterance("Yo sere tu traductor y gracias por usar mi codigo")); 
+
+//**********************************************************
+//         CONFIGURACION DE VOZ DE API GOOGLE
+//**********************************************************
+// 0 Google Deutsch true
+// 1 Google US English 
+// 2 Google UK English Female 
+// 3 Google UK English Male 
+// 4 Google español 
+// 5 Google español de Estados Unidos 
+// 11 Google 한국의 
+// 12 Google Nederlands 
+// 13 Google polski 
+// 14 Google português do Brasil 
+// 15 Google русский 
+// 16 Google 普通话（中国大陆） 
+// 17 Google 粤語（香港） 
+// 18 Google 國語（臺灣）
+// **************************************************
+// cargue el idioma deseado segun su numero
+
+Google = 5
+
+msg.voice = voices[Google];
+
+// **************************************************
+// otras configuraciones:
+
 msg.rate = 1.19; // 0.1 to 2
+msg.volume = 1; // 0 to 1
 msg.pitch = 0; //0 to 2
+
+// **************************************************
+
 
 function check() {
   try {
@@ -44,7 +75,3 @@ function check() {
 window.i = setInterval(check, 600);
 
 
-
-// ************************youtube ******************************
-// 'p[data-purpose="transcript-cue-active"] font')[1].innerHTML;
-// document.querySelectorAll('span[class=captions-text] span[class= caption-visual-line]')[0].innerText
